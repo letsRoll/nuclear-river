2c019fee623c3c7c42510de1e817f07284e06afb
Denis Ivanov <d.ivanov@2gis.ru>
2016-06-15 11:19:28 +0600

    output redirect experiments

--- build/installhosts.ps1
+++ build/installhosts.ps1
@@ -38,7 +38,7 @@ Task Run-InstallHosts -Precondition { $Metadata['HostsToInstall'] } {
 			& $psExec ('\\' + $targetHost) -accepteula -u 'NT AUTHORITY\NETWORK SERVICE' -cf $setupExe | Out-Host
 
 			$session = Get-CachedSession $targetHost
-			 $job = Invoke-Command $session {
+			 Invoke-Command $session {
 		
 				$servicePath = "${Env:WinDir}\ServiceProfiles\NetworkService\AppData\Local\$using:packageId"
 				$appPath = Get-ChildItem $servicePath | where { $_.PSIsContainer } | select -First 1
@@ -69,12 +69,9 @@ Task Run-InstallHosts -Precondition { $Metadata['HostsToInstall'] } {
 				if ($LastExitCode -ne 0) {
 					throw "Command failed with exit code $LastExitCode"
 				}
-			} -AsJob
 
-			Wait-Job $job
-			$output = Receive-Job $job 
-
-			$output
+				& ping
+			}
 		}
 	}
 }

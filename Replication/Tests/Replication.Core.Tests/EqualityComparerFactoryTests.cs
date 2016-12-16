e0f9204acb61d25501f3d46879f47a9e4dfce1e8
a.rechkalov <a.rechkalov@2gis.ru>
2016-12-16 11:54:24 +0700

    Доработал EqualityComparerFactory
    Добавил возможность использовать сторонние IEqualityComparer для сравнения полей.

--- Replication/Tests/Replication.Core.Tests/EqualityComparerFactoryTests.cs
+++ Replication/Tests/Replication.Core.Tests/EqualityComparerFactoryTests.cs
@@ -47,5 +47,38 @@ namespace NuClear.Replication.Core.Tests
 
             Assert.That(comparer.GetHashCode(left), Is.EqualTo(left.GetHashCode()));
         }
+
+        [TestCase(1, -1, false)]
+        [TestCase(-1, -1, true)]
+        [TestCase(-1, -2, true)]
+        [TestCase(1, 1, true)]
+        public void EqualityShouldRespectProvidedComparers(int idLeft, int idRight, bool expected)
+        {
+            var provider = new Mock<IObjectPropertyProvider>();
+            var props = typeof(SampleEntity).GetProperties();
+            provider.Setup(x => x.GetProperties<SampleEntity>()).Returns(props);
+            provider.Setup(x => x.GetPrimaryKeyProperties<SampleEntity>()).Returns(new List<PropertyInfo>());
+
+            var factory = new EqualityComparerFactory(provider.Object, new CustomIntegerComparer());
+            var comparer = factory.CreateCompleteComparer<SampleEntity>();
+
+            var left = new SampleEntity { Id = idLeft };
+            var right = new SampleEntity { Id = idRight };
+
+            Assert.AreEqual(expected, comparer.GetHashCode(left) == comparer.GetHashCode(right));
+            Assert.AreEqual(expected, comparer.Equals(left, right));
+        }
+
+        /// <summary>
+        /// Считает все отрицательные числа равными.
+        /// </summary>
+        private class CustomIntegerComparer : IEqualityComparer<int>
+        {
+            public bool Equals(int x, int y)
+                => x < 0 && y < 0 || x == y;
+
+            public int GetHashCode(int obj)
+                => obj < 0 ? -1 : obj;
+        }
     }
 }

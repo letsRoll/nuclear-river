344b59d14eade3f802a58e288fa25bb4d7f9161f
a.rechkalov <a.rechkalov@2gis.ru>
2017-03-17 16:44:36 +0700

    Переделал загрузку типов

--- StateInitialization/StateInitialization.Core/Factories/StaticAccessorTypesProvider.cs
+++ StateInitialization/StateInitialization.Core/Factories/StaticAccessorTypesProvider.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 
 using NuClear.Replication.Core.DataObjects;
 
@@ -8,17 +9,33 @@ namespace NuClear.StateInitialization.Core.Factories
 {
     public sealed class StaticAccessorTypesProvider : IAccessorTypesProvider
     {
-        private static readonly IReadOnlyDictionary<Type, Type[]> AccessorTypes =
-            (from type in AppDomain.CurrentDomain.GetAssemblies().Where(x => !x.IsDynamic).SelectMany(x => x.ExportedTypes)
-             from @interface in type.GetInterfaces()
-             where !type.IsAbstract && @interface.IsGenericType && @interface.GetGenericTypeDefinition() == typeof(IStorageBasedDataObjectAccessor<>)
-             select new { GenericArgument = @interface.GetGenericArguments()[0], Type = type })
-            .GroupBy(x => x.GenericArgument, x => x.Type)
-            .ToDictionary(x => x.Key, x => x.ToArray());
+        private static readonly Lazy<IReadOnlyDictionary<Type, Type[]>> AccessorTypes = new Lazy<IReadOnlyDictionary<Type, Type[]>>(LoadAccessorTypes);
+
+        private static IReadOnlyDictionary<Type, Type[]> LoadAccessorTypes()
+            => AppDomain.CurrentDomain.GetAssemblies()
+                        .Where(x => !x.IsDynamic)
+                        .SelectMany(SafeGetAssemblyExportedTypes)
+                        .SelectMany(type => type.GetInterfaces(), (type, @interface) => new { type, @interface })
+                        .Where(x => !x.type.IsAbstract && x.@interface.IsGenericType && x.@interface.GetGenericTypeDefinition() == typeof(IStorageBasedDataObjectAccessor<>))
+                        .Select(x => new { GenericArgument = x.@interface.GetGenericArguments()[0], Type = x.type })
+                        .GroupBy(x => x.GenericArgument, x => x.Type)
+                        .ToDictionary(x => x.Key, x => x.ToArray());
+
+        private static IEnumerable<Type> SafeGetAssemblyExportedTypes(Assembly assembly)
+        {
+            try
+            {
+                return assembly.ExportedTypes;
+            }
+            catch
+            {
+                return Array.Empty<Type>();
+            }
+        }
 
         public IReadOnlyCollection<Type> GetAccessorsFor(Type dataObjectType)
         {
-            return AccessorTypes[dataObjectType];
+            return AccessorTypes.Value[dataObjectType];
         }
     }
 }
